624ec6a2ae12184c9c409ae1c17537d9df1a29b7
ybadache <yassine.badache@gmail.com>
2022-02-26 15:42:58 +0100

    :sparkles: File reading my mock menu list

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) =>  {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World !');
+    fs.readFile('data.json', function (error, data)   {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.write(data);
+        return res.end();
+    });
 });
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}:`);
 });
+
+// Elegantlt terminating node.js process
+process.on('SIGTERM', () => {
+    server.close(() => {
+        console.log('Process terminated')
+    })
+})
